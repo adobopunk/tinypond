1c79366d4cad291092c03922ee3c81676794fbd7
TJ Lien <89636647+tjlien@users.noreply.github.com>
2024-07-09 18:46:44 -0700

    Initial HTML and CSS

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log("what's up guys");
+window.onload = function () {
+	// Get the current year
+	const currentYear = new Date().getFullYear();
+
+	// Get the span element
+	const yearSpan = document.getElementById('currentYear');
+
+	// Update the span element with the current year
+	yearSpan.textContent = currentYear;
+};
