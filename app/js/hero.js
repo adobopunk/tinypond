b6a89e79312a98044df5efa43d458f8a1a8713d7
TJ Lien <89636647+tjlien@users.noreply.github.com>
2024-08-09 16:26:54 -0700

    misc work on whole site and about page commit before I screw up everything

---
+++ app/js/hero.js
@@ -0,0 +1,30 @@
+const textObject = document.getElementById('hero__title');
+const words = ['Visuals.', 'Branding.', 'Animation.', 'Events.']; // array of words
+let currentWordIndex = 1;
+let opacity = 1;
+
+let animationDirection = -1; // -1 for fade out, 1 for fade in
+
+function animate() {
+    opacity += animationDirection * 0.01;
+
+	textObject.style.opacity = opacity;
+
+	if (opacity <= 0 && animationDirection === -1) {
+		// fade out complete, update text and start fade in
+
+		textObject.textContent = words[currentWordIndex];
+
+		currentWordIndex = (currentWordIndex + 1) % words.length;
+
+		animationDirection = 1;
+	} else if (opacity >= 1 && animationDirection === 1) {
+		// fade in complete, start fade out
+
+		animationDirection = -1;
+	}
+
+	requestAnimationFrame(animate);
+}
+
+setTimeout(animate, 1000); // delay the animation by 500ms
