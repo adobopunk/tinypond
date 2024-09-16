fb652bdfb352ef6b3f35051df3b31f668c58597e
TJ Lien <89636647+tjlien@users.noreply.github.com>
2024-09-16 00:57:28 -0700

    project page updates

--- app/js/footer.js
+++ app/js/footer.js
@@ -12,10 +12,9 @@ window.onload = function () {
 	const footerTag = document.querySelector('h2.footer__title');
 	const footerOptions = [
 		"Your Journey Awaits.<br>Let's Get Started!",
-		'The Design Studio<br>For High Impact Events.',
+		'The Creative Agency<br>For High Impact Events.',
 		"Tiny Pond, Big Results.<br>Let's Dive In.",
 		'Free Consult?<br>We Got You.',
-		"You Have Ideas. We Have Ideas.<br>Let's Make Them Happen.",
 	];
 
 	const randomFooter = function () {
