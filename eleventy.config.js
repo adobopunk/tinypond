c8115f60df2ba7e5b1d6d8a6516eb69d9a1ae9b1
TJ Lien <89636647+tjlien@users.noreply.github.com>
2024-10-03 19:53:01 -0700

    added initial SEO and metadata

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,25 @@
+const sitemap = require('@quasibit/eleventy-plugin-sitemap');
+
 module.exports = function (eleventyConfig) {
-	//copy `assets/` to `_site/assets/`
+	eleventyConfig.addPlugin(sitemap, {
+		sitemap: {
+			hostname: 'https://tinypond.studio',
+		},
+	});
 	eleventyConfig.addPassthroughCopy('assets');
 	eleventyConfig.addPassthroughCopy('fonts');
 	eleventyConfig.addPassthroughCopy('img');
 	eleventyConfig.addPassthroughCopy('app');
+	eleventyConfig.addPassthroughCopy('src/robots.txt');
 
 	return {
 		dir: {
 			input: 'src',
 			output: '_site',
 			includes: '_templates',
+		},
+		data: {
+			siteUrl: 'https://tinypond.studio',
 		},
 	};
 };
