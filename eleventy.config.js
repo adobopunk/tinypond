af42d1b44293fc3ffaa47587d4fb4b6d109e5338
TJ Lien <tjlien@pm.me>
2024-10-06 10:45:15 -0700

    added top margin to fullwidth feature text

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,4 @@
-const sitemap = require('@quasibit/eleventy-plugin-sitemap');
-
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(sitemap, {
-		sitemap: {
-			hostname: 'https://tinypond.studio',
-		},
-	});
 	eleventyConfig.addPassthroughCopy('assets');
 	eleventyConfig.addPassthroughCopy('fonts');
 	eleventyConfig.addPassthroughCopy('img');
