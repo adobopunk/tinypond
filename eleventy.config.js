7be3b8da3e6f8005de7b4a8eb941fa7c88a9d819
TJ Lien <tjlien@pm.me>
2024-11-13 19:02:09 -0800

    refactored gallery section, added order frontmatter to pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,25 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('app');
 	eleventyConfig.addPassthroughCopy('src/robots.txt');
 
+	eleventyConfig.addCollection('projects', function (collectionApi) {
+		return collectionApi
+			.getFilteredByGlob('src/projects/*.html')
+			.sort((a, b) => {
+				const orderA =
+					a.data.order !== undefined ? a.data.order : Infinity;
+				const orderB =
+					b.data.order !== undefined ? b.data.order : Infinity;
+
+				// If `order` exists in both, sort by `order`
+				if (orderA !== orderB) {
+					return orderA - orderB;
+				}
+
+				// If `order` is the same or missing, sort by `date`
+				return new Date(b.data.date) - new Date(a.data.date);
+			});
+	});
+
 	return {
 		dir: {
 			input: 'src',
