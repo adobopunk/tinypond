604e4fee5995bf864f472e5d5895ae17e730059f
TJ Lien <tjlien@pm.me>
2024-11-22 20:08:58 -0800

    add secondary gallery and infinite project scroll

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,24 +5,57 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('app');
 	eleventyConfig.addPassthroughCopy('src/robots.txt');
 
-	eleventyConfig.addCollection('featured', function (collectionApi) {
+	eleventyConfig.addCollection('projects', function (collectionApi) {
 		return collectionApi
-			.getFilteredByGlob('src/featured/*.html')
+			.getFilteredByGlob('src/projects/*.html')
 			.sort((a, b) => {
+				// First priority: Check for important: yes
+				const isFeaturedA = a.data.featured === 'yes';
+				const isFeaturedB = b.data.featured === 'yes';
+
+				if (isFeaturedA !== isFeaturedB) {
+					return isFeaturedA ? -1 : 1;
+				}
+
+				// Second priority: Check order
 				const orderA =
 					a.data.order !== undefined ? a.data.order : Infinity;
 				const orderB =
 					b.data.order !== undefined ? b.data.order : Infinity;
 
-				// If `order` exists in both, sort by `order`
 				if (orderA !== orderB) {
 					return orderA - orderB;
 				}
 
-				// If `order` is the same or missing, sort by `date`
+				// Third priority: Sort by date
 				return new Date(b.data.date) - new Date(a.data.date);
 			});
 	});
+
+	eleventyConfig.addCollection(
+		'nonFeaturedProjects',
+		function (collectionApi) {
+			return collectionApi
+				.getFilteredByGlob('src/projects/*.html')
+				.filter((item) => item.data.featured !== 'yes')
+				.sort((a, b) => {
+					// First priority: Check order
+					const orderA =
+						a.data.order !== undefined
+							? a.data.order
+							: Infinity;
+					const orderB =
+						b.data.order !== undefined
+							? b.data.order
+							: Infinity;
+					if (orderA !== orderB) {
+						return orderA - orderB;
+					}
+					// Second priority: Sort by date
+					return new Date(b.data.date) - new Date(a.data.date);
+				});
+		}
+	);
 
 	return {
 		dir: {
